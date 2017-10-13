0bc3b98d88043d1373c93024bc7e21bab8c5d3c7
Lienhart Woitok <lienhart.woitok@netlogix.de>
2017-10-13 19:08:55 +0200

    [TASK] Fix allow node cleanup for pupept4

--- manifests/allow_cert_cleanup.pp
+++ manifests/allow_cert_cleanup.pp
@@ -1,14 +1,34 @@
 class puppetmaster::allow_cert_cleanup (
-  $allowed_nodes = '*',
+	$allowed_nodes = '*',
 ) {
-  puppetmaster::authrule { 'cert-cleanup':
-    comment => '# Allows nodes to clean up certificates of any node. This is being triggered
+	if versioncmp($::clientversion, '4.0.0') < 0 {
+		puppetmaster::authrule { 'cert-cleanup':
+			comment => '# Allows nodes to clean up certificates of any node. This is being triggered
       # at provisioning time in kickstart to remove the old certificate, since a new
       # one is generated when the system is provisioned.',
-    path => '/certificate_status/',
-    auth => 'any',
-    method => 'find, save, destroy',
-    allow => $allowed_nodes,
-    order => 810,
-  }
+			path => '/certificate_status/',
+			auth => 'any',
+			method => 'find, save, destroy',
+			allow => $allowed_nodes,
+			order => 810,
+		}
+	} else {
+		hocon_setting { 'cert-cleanup':
+			path => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
+			setting => 'authorization.rules',
+			type => 'array_element',
+			value => {
+				'match-request' => {
+					'path' => '/puppet-ca/v1/certificate_status',
+					'type' => 'path',
+					'method' => 'delete',
+				},
+				'allow-unauthenticated' => true,
+				'sort-order' => 500,
+				'name' => 'allow-cert-cleanup',
+			},
+			notify => Service['puppetserver'],
+		}
+	}
+
 }
